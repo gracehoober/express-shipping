2d3c9377677c5170f20023cb5eb1a7df90466243
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-09-19 17:57:43 -0700

    started on mock

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,20 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const fetchMock = require("fetch-mock");
+const { SHIPIT_SHIP_URL, shipProduct} = require("./shipItApi")
 
+//shipItAPI.shipProduct = jest.fn();
 
 test("shipProduct", async function () {
+  fetchMock.get(`${SHIPIT_SHIP_URL}`, 4)
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
     zip: "12345-6789",
   });
+  const res = await shipProduct(4)
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(4);
 });
